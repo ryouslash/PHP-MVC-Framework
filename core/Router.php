d23ec3689779dbcbc8c31f964521b7ab474ead38
池田 遼 <ryouslash@gmail.com>
2025-01-25 10:42:55 +0400

    SIteControllerの追加

--- core/Router.php
+++ core/Router.php
@@ -5,16 +5,23 @@ namespace app\core;
 class Router
 {
   public Request $request;
+  public Response $response;
   protected array $routes = [];
 
-  public function __construct(Request $request)
+  public function __construct(Request $request, Response $response)
   {
     $this->request = $request;
+    $this->response = $response;
   }
 
   public function get($path, $callback)
   {
     $this->routes['get'][$path] = $callback;
+  }
+
+  public function post($path, $callback)
+  {
+    $this->routes['post'][$path] = $callback;
   }
 
   public function resolve()
@@ -23,7 +30,8 @@ class Router
     $method = $this->request->getMethod();
     $callback = $this->routes[$method][$path] ?? false;
     if ($callback === false) {
-      return "Not found";
+      $this->response->setStatusCode(404);
+      return $this->renderView("_404");
     }
     if (is_string($callback)) {
       return $this->renderView($callback);
@@ -31,10 +39,10 @@ class Router
     return call_user_func($callback);
   }
 
-  public function renderView($view)
+  public function renderView($view, $params = [])
   {
     $layoutContent = $this->layoutContent();
-    $viewContent = $this->renderOnlyView($view);
+    $viewContent = $this->renderOnlyView($view, $params);
     return str_replace('{{content}}', $viewContent, $layoutContent);
   }
 
@@ -45,8 +53,11 @@ class Router
     return ob_get_clean();
   }
 
-  protected function renderOnlyView($view)
+  protected function renderOnlyView($view, $params)
   {
+    foreach ($params as $key => $value) {
+      $$key = $value;
+    }
     ob_start();
     include_once Application::$ROOT_DIR . "/views/$view.php";
     return ob_get_clean();
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,7 +18,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/contact">Contact</a>
@@ -30,7 +30,9 @@
     </div>
   </nav>
 
-  {{content}}
+  <div class="container">
+    {{content}}
+  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
