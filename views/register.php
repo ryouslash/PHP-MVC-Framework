14e14b3b100ab651e5231ce267ac966efde2b24c
池田 遼 <ryouslash@gmail.com>
2025-02-13 17:22:23 +0400

    registerフォームにてバリデーションエラーの表示の効率化

--- views/register.php
+++ views/register.php
@@ -1,34 +1,15 @@
 <h1>Create an account</h1>
-
-<form action="" method="post">
-  <div class="row">
-    <div class="col">
-      <div class="mb-3">
-        <label>Firstname</label>
-        <input type="text" name="firstname" class="form-control<?php echo $model->hasError('firstname') ? ' is-invalid' : ''; ?>" value="<?php echo $model->firstname; ?>">
-        <div class="invalid-feedback"><?php echo $model->getFirstError('firstname'); ?></div>
-      </div>
-
-    </div>
-    <div class="col">
-      <div class="mb-3">
-        <label>Lastname</label>
-        <input type="text" name="lastname" class="form-control">
-      </div>
-    </div>
+<?php $form = \app\core\form\Form::begin('', "post") ?>
+<div class="row">
+  <div class="col">
+    <?php echo $form->field($model, 'firstname') ?>
   </div>
-  <div class="mb-3">
-    <label>Email</label>
-    <input type="text" name="email" class="form-control">
+  <div class="col">
+    <?php echo $form->field($model, 'lastname') ?>
   </div>
-  <div class="mb-3">
-    <label>Password</label>
-    <input type="password" name="password" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label>Confirm Password</label>
-    <input type="password" name="confirmPassword" class="form-control">
-  </div>
-
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+</div>
+<?php echo $form->field($model, 'email') ?>
+<?php echo $form->field($model, 'password') ?>
+<?php echo $form->field($model, 'confirmPassword') ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+<?php $form = \app\core\form\Form::end() ?>
