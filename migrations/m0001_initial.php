fb2c2f776396e323c251032d208e91b7ad188901
池田 遼 <ryouslash@gmail.com>
2025-03-01 10:06:25 +0400

    ユーザー登録機能の作成

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -11,8 +11,8 @@ class m0001_initial
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL,
-    lasttname VARCHAR(255) NOT NULL,
-    status TINYINT NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    status TINYINT NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     ) ENGINE=INNODB;";
     $db->pdo->exec($SQL);
